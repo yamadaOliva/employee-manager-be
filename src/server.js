488c0757a03ad3d5e137af0abb5aa6ef50ab259e
yamadaOliva <vietanh87.forwork@gmail.com>
2023-03-03 21:47:55 +0700

    test server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
+require("dotenv").config();
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 // config view engine
 configViewEngine(app);
 
 // init all web routes
 initWebRoutes(app);
 
-const PORT = 8080;
+
 app.listen(PORT,(err)=>{
     console.log("Server is running on port: " + PORT);
 });
